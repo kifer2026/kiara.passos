51d7ef6ce17c35b7d3bbafa62cef16b1222a5d56
kifer2026 <kiara.passos@escola.pr.gov.br>
2026-06-01 10:45:43 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function mostrarMensagem() {
-    const mensagem = document.getElementById("mensagem");
-
-    mensagem.innerHTML =
-        "🌱 O Agro Forte investe em inovação, produtividade e sustentabilidade para alimentar o futuro!";
-}
